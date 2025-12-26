96244bc0068de18e4b8e87fbea7f5a32543409c4
Sangamithradillibabu <sangamithra.dillibabu@gmail.com>
2025-12-26 20:54:21 +0530

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -3,6 +3,8 @@ from backend.text_to_sql_langgraph_flow import run_text_to_sql
 response = run_text_to_sql(
     user_input=" DELETE info of MEENA",
     role="ADMIN"
+    user_id=103
 )
 
 print(response)
+
