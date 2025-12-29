9c50d7f25694c589d1a7ea9c28c024bd2057a41b
Sangamithradillibabu <sangamithra.dillibabu@gmail.com>
2025-12-29 14:22:50 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DB_PATH = BASE_DIR / "database" / "text_to_sql.db"
 USER_DB_PATH = BASE_DIR / "database" / "user.db"
 
-OPENROUTER_API_KEY ="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"
+OPENROUTER_API_KEY =" PUT API KEY HERE"
 OPENROUTER_MODEL = "gpt-4o-mini"
+
 
